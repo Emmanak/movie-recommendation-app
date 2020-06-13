bd1d8d6f446bd3eb73278ef45546ef13de5e0b72
Emmanuel Augustine <emmanuelaugustine@hotmail.com>
2020-06-12 17:23:40 -0700

    Added animations to movie list cards

--- src/components/movieList.tsx
+++ src/components/movieList.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import MovieCard from './moviecard';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+
 
 export interface MovieListProps {
     movieList: Array<any>
@@ -32,17 +34,19 @@ class MovieList extends React.Component<MovieListProps, MovieListState> {
         console.log(this.props.movieList)
         if(this.state.update === true){
             return (
+                <TransitionGroup>
                 <div id='movieList' className="container">
                     <div id='movies' className="row pb-5">
-                        {this.props.movieList.map(movie => <div id={movie.id} key={"movie-"+movie.id.toString()} className="d-flex justify-content-around col-md-4 col-sm-6 col-xs-12 pt-5"><MovieCard 
+                        {this.props.movieList.map(movie =><div id={movie.id} key={"movie-"+movie.id.toString()} className="d-flex justify-content-around col-md-4 col-sm-6 col-xs-12 pt-5"><CSSTransition in={true} appear={true} key={movie.id} timeout={1500} classNames="card"><MovieCard 
                         key={movie.id}
                         id={"card-"+movie.id}
                         movieName={movie.title} 
                         image={"https://image.tmdb.org/t/p/w500"+movie.poster_path}
-                        overview={movie.overview}/></div>)}
+                        overview={movie.overview}/>
+                        </CSSTransition></div>)}
                     </div>
-                    
                 </div>
+                </TransitionGroup>
              );
         }
         else{
